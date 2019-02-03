1bbbe3cc8110da6214914dcac062b02b06338a04
Dustin Ramdass <dramd010@fiu.edu>
2019-02-03 17:13:25 -0500

    First Commit Of GeekText Skeleton Project

---
+++ GeekText/BookDetails.aspx
@@ -0,0 +1,15 @@
+﻿<%@ Page Title="Book Details" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="BookDetails.aspx.cs" Inherits="GeekText.About" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <h2><%: Title %>.</h2>
+    <h3>Current Books in Database</h3>
+    <div id ="BookDisplayDetailsDiv">
+        <asp:GridView ID ="BookDetailsGridView" runat ="server" AutoGenerateColumns ="false">
+            <Columns>
+                <asp:BoundField DataField ="Name" HeaderText="BookName" ItemStyle-Width="150px" />
+                <asp:BoundField DataField ="Author" HeaderText="Author" ItemStyle-Width="150px" />
+                <asp:BoundField DataField ="Genre" HeaderText="Genre" ItemStyle-Width="150px" />
+            </Columns>
+        </asp:GridView>
+    </div>
+</asp:Content>
---
+++ GeekText/BookDetails.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace GeekText
+{
+    public partial class About : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!this.IsPostBack)
+            {
+                string constr = ConfigurationManager.ConnectionStrings["GeekTextConnection"].ConnectionString;
+                string query = "Select * from Books;";
+
+                using (SqlConnection con = new SqlConnection(constr))
+                {
+                    using (SqlCommand cmd = new SqlCommand(query))
+                    {
+                        using (SqlDataAdapter sda = new SqlDataAdapter())
+                        {
+                            cmd.Connection = con;
+                            sda.SelectCommand = cmd;
+                            using (DataSet ds = new DataSet())
+                            {
+                                sda.Fill(ds);
+                                BookDetailsGridView.DataSource = ds.Tables[0];
+                                BookDetailsGridView.DataBind();
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        
+    }
+}
